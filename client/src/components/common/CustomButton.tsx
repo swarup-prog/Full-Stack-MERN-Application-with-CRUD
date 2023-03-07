6fd9c36e5161cb68113b7644505ac0413a49bc94
swarup-prog <lswarup1234@gmail.com>
2023-03-07 08:49:07 +0545

    Updated Dashboard Piechart

---
+++ client/src/components/common/CustomButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomButoon = () => {
+  return (
+    <div>CustomButoon</div>
+  )
+}
+
+export default CustomButoon
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import { useList } from '@pankod/refine-core';
+import { Typography, Box, Stack } from '@pankod/refine-mui';
+
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent
+} from 'components';
 
 const home = () => {
   return (
-    <div>
-      home
-    </div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart 
+          title="Properties for Sale" 
+          value={684} 
+          series={[75, 25]} 
+          colors={['#275be8', '#c4e8ef']}
+        />
+        <PieChart 
+          title="Properties for Rent" 
+          value={550} 
+          series={[60, 40]} 
+          colors={['#275be8', '#c4e8ef']}
+        />
+        <PieChart 
+          title="Total Customers" 
+          value={5684} 
+          series={[75, 25]} 
+          colors={['#275be8', '#c4e8ef']}
+        />
+        <PieChart 
+          title="Total Cities" 
+          value={555} 
+          series={[75, 25]} 
+          colors={['#275be8', '#c4e8ef']}
+        />
+      </Box>
+    </Box>
   )
 }
 
