38a600ee397c0901694a5cdd649bc02aadf55037
swarup-prog <lswarup1234@gmail.com>
2023-03-07 20:33:38 +0545

    Added custom button

--- client/src/components/common/CustomButton.tsx
+++ client/src/components/common/CustomButton.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Button, minWidth } from '@pankod/refine-mui';
 
-const CustomButoon = () => {
+import { CustomButtonProps } from 'interfaces/common';
+
+const CustomButton = ({ type, title, backgroundColor, color, fullWidth, icon, handleClick }: CustomButtonProps) => {
   return (
-    <div>CustomButoon</div>
+    <Button
+      sx={{
+        flex: fullWidth ? 1 : 'unset',
+        padding: '10px 15px',
+        width: fullWidth ? '100%' : 'fit-content',
+        minWidth: 130,
+        backgroundColor,
+        color,
+        fontSize: 16,
+        fontWeight: 600,
+        gap: '10px',
+        textTransform: 'capitalize',
+        '&:hover': {
+          opacity:0.9,
+          backgroundColor 
+        }
+      }}
+      onClick={handleClick}
+    >
+      {icon}
+      {title}
+    </Button>
   )
 }
 
-export default CustomButoon
+export default CustomButton
